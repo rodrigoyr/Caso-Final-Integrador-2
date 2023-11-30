e6542ee6e5a0baa433478955ebf6dc34a8c4693d
rodrigoyr <ryeperub@myuax.com>
2023-11-30 11:49:20 +0100

    trying new code commit

--- Ejercicio/main.cpp
+++ Ejercicio/main.cpp
@@ -1,113 +1,6 @@
 #include <iostream>
-#include <memory>
-#include <vector>
-#include <string>
 #include <fstream>
-#include <stdexcept>
-
-using namespace std;
-
-struct ColorConsole
-{
-    static constexpr auto fg_blue = "\033[34m";
-    static constexpr auto bg_white = "\033[47m";
-};
-
-class ConsoleBox
-{
-public:
-    void new_text() { /*...*/ }
-    void set_text(const string& text) const { cout << text << endl; }
-};
-
-vector<unique_ptr<ConsoleBox>> consoleBoxes;
-
-void load_script(const string& filename, bool show_script = false)
-{
-    try
-    {
-        ifstream file(filename);
-
-        if (!file.is_open())
-        {
-            throw runtime_error("Error de apertura de " + filename);
-        }
-
-        string script((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
-
-        if (show_script)
-        {
-            cout << ColorConsole::fg_blue << ColorConsole::bg_white;
-            cout << script << endl;
-        }
-
-        consoleBoxes.emplace_back(make_unique<ConsoleBox>());
-        auto& currentConsoleBox = *consoleBoxes.back();
-        currentConsoleBox.new_text();
-        currentConsoleBox.set_text(script);
-    }
-    catch (const exception& e)
-    {
-        cerr << "Error durante la lectura del archivo: " << e.what() << endl;
-    }
-}
-
-void load_script()
-{
-    string filename;
-    cout << "Introduce el nombre del archivo: ";
-    cin >> filename;
-    load_script(filename, true);
-}
-
-int main()
-{
-    load_script();
-    return 0;
-}
-
-#include <iostream>
-#include <memory>
-#include <vector>
 #include <string>
-#include <fstream>
-#include <stdexcept>
-
-using namespace std;
-
-struct ColorConsole
-{
-    static constexpr auto fg_blue = "\033[34m";
-    static constexpr auto bg_white = "\033[47m";
-};
-
-class ConsoleBox
-{
-public:
-    void new_text() { /*...*/ }
-    void set_text(const string& text) const { cout << text << endl; }
-};
-
-vector<unique_ptr<ConsoleBox>> consoleBoxes;
-
-void load_script(const string& filename, bool show_script = false)
-{
-    try
-    {
-        ifstream file(filename);
-
-        if (!file.is_open())
-        {
-            throw runtime_error("Error de apertura de " + filename);
-        }
-
-        string script((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
-
-        if
-#include <memory>
-#include <vector>
-#include <string>
-#include <fstream>
 
 using namespace std;
 
@@ -119,30 +12,34 @@ struct ColorConsole
 
 struct ConsoleBox
 {
-    void new_text() { /*...*/ }
-    void set_text(const string& text) { cout << text << endl; }
+private:
+    void new_text() {/*...*/}
+public:
+    void set_text(const string &text) { cout << text << endl; }
 };
 
-vector<unique_ptr<ConsoleBox>> consoleBoxes;
+ConsoleBox *consoleBox = new ConsoleBox;
 
-void load_script(const string& filename, bool show_script = false)
+void load_script(const char* filename, bool show_script = false)
 {
     string script;
-    ifstream file(filename);
+    ifstream file(filename, ios::in);
+
+    if (!file.is_open())
+    {
+        cerr << "Error de apertura de " << filename << endl;
+        return;
+    }
 
     try
     {
-        if (!file.is_open())
+        string line;
+        while (getline(file, line))
         {
-            throw ios_base::failure("Error de apertura de " + filename);
+            script += line + "\n";
         }
 
-        char buf[4001];
-        while (file.read(buf, sizeof(buf) - 1))
-        {
-            buf[file.gcount()] = 0;
-            script.append(buf);
-        }
+        file.close();
 
         if (show_script)
         {
@@ -150,81 +47,26 @@ void load_script(const string& filename, bool show_script = false)
             cout << script << endl;
         }
 
-        consoleBoxes.emplace_back(make_unique<ConsoleBox>());
-        auto& currentConsoleBox = *consoleBoxes.back();
-        currentConsoleBox.new_text();
-        currentConsoleBox.set_text(script);
-    }
-    catch (const ios_base::failure& e)
-    {
-        cerr << "Error durante la lectura del archivo: " << e.what() << endl;
+        consoleBox->new_text();
+        consoleBox->set_text(script);
     }
     catch (const exception& e)
     {
-#include <iostream>
-#include <memory>
-#include <vector>
-#include <string>
-#include <fstream>
-#include <stdexcept>
+        cerr << "Error durante la lectura del archivo: " << e.what() << endl;
+        file.close();
+    }
+}
 
-        using namespace std;
+void load_script()
+{
+    char filename[500];
+    cout << "Archivo: ";
+    cin.getline(filename, sizeof(filename));
+    load_script(filename, true);
+}
 
-        struct ColorConsole
-        {
-            static constexpr auto fg_blue = "\033[34m";
-            static constexpr auto bg_white = "\033[47m";
-        };
-
-        class ConsoleBox
-        {
-        public:
-            void new_text() { /*...*/ }
-            void set_text(const string& text) const { cout << text << endl; }
-        };
-
-        vector<unique_ptr<ConsoleBox>> consoleBoxes;
-
-        void load_script(const string& filename, bool show_script = false)
-        {
-            try
-            {
-                ifstream file(filename);
-
-                if (!file.is_open())
-                {
-                    throw runtime_error("Error de apertura de " + filename);
-                }
-
-                string script((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
-
-                if (show_script)
-                {
-                    cout << ColorConsole::fg_blue << ColorConsole::bg_white;
-                    cout << script << endl;
-                }
-
-                consoleBoxes.emplace_back(make_unique<ConsoleBox>());
-                auto& currentConsoleBox = *consoleBoxes.back();
-                currentConsoleBox.new_text();
-                currentConsoleBox.set_text(script);
-            }
-            catch (const exception& e)
-            {
-                cerr << "Error durante la lectura del archivo: " << e.what() << endl;
-            }
-        }
-
-        void load_script()
-        {
-            string filename;
-            cout << "Introduce el nombre del archivo: ";
-            cin >> filename;
-            load_script(filename, true);
-        }
-
-        int main()
-        {
-            load_script();
-            return 0;
-        }
+int main()
+{
+    load_script();
+    return 0;
+}
